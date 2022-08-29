613921a43c9cdfca8c5c60920538f54ea19e6aa3
Nityam Bhojani <78067067+niityam@users.noreply.github.com>
2022-08-29 18:54:02 +0530

    Delete src directory

--- src/prac3.java
+++
@@ -1,6 +0,0 @@
-public class prac3 {
-    public static void main(String args[]) {
-              int cost  = (40*6) + (10*12);
-              System.out.println("Total cost incurred is:  " +cost);
-            }
-}
