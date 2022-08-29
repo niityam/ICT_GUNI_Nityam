7268be3bcfcd2e0633dc467f04124a25e0e2c6f6
Nityam Bhojani <78067067+niityam@users.noreply.github.com>
2022-08-29 18:52:06 +0530

    OOP Sem 3 java practicals

---
+++ src/prac1.java
@@ -0,0 +1,7 @@
+public class prac1 {
+    public static void main(String args[]) {
+      int diameter = 20;
+      System.out.println("Perimeter of pizza would be:  " + (22*diameter)/7);
+      System.out.println("Area of pizza would be:  " + (22*(diameter/2)*(diameter/2))/7);
+    }
+}
---
+++ src/prac2.java
@@ -0,0 +1,7 @@
+public class prac2 {
+    public static void main(String args[]) {
+              double a;
+              a = 2.0*(1-(3.0/3)+(1.0/5)-(2.0/14)+(1.0/12)-(2.0/22));
+              System.out.println("Solution is:  " +a);
+            }
+}
