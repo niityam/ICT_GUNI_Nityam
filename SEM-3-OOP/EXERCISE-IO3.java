59147e2803dd452f682803f572de30ecbda08198
Nityam Bhojani <78067067+niityam@users.noreply.github.com>
2022-09-12 18:20:23 +0530

    Exercise - Input Output 3
    Create an arithmetic calculator application which input 2 numbers and generate output of all the arithmetic operations

---
+++ SEM-3-OOP/EXERCISE-IO3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class EXERCISEIO3
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int x, y;
+	    x = sc.nextInt();
+	    y = sc.nextInt();
+	    System.out.println("Addition is: "+(x+y));
+	    System.out.println("Substraction is: "+(x-y));
+	    System.out.println("Multiplication is: "+(x*y));
+	    System.out.println("Division is: "+(x/y));
+        sc.close();
+	}
+}
