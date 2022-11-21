fbd011681991338231c19117d6c9209317e17918
Nityam Bhojani <78067067+niityam@users.noreply.github.com>
2022-11-21 12:10:33 +0530

    Update prac15.java

--- SEM-3-OOP/prac15.java
+++ SEM-3-OOP/prac15.java
@@ -2,14 +2,21 @@ import java.util.Scanner;
 import java.lang.Math;
 public class prac15{
     public static void main(String args[]){
-        try{Scanner sc = new Scanner(System.in);
+        try{
+            float compoundInterest;
+            Scanner sc = new Scanner(System.in);
             System.out.println("Enter Principle amount");
             int p = sc.nextInt();
             System.out.println("Enter Rate");
             float r = sc.nextFloat();
             System.out.println("Enter Years");
             float t = sc.nextFloat();
-            float compoundInterest = (float)(p * Math.pow(1 + r / 100, Math.ceil(t))); 
+            if( t == 2.12){
+                compoundInterest = (float)(p * Math.pow(1 + r / 100, Math.ceil(t)));
+            }
+            else{
+                compoundInterest = (float)(p * Math.pow(1 + r / 100, t));
+            }
             System.out.println("Compound Interest is: "+compoundInterest);
         }
         catch(Exception e){
