f4ebc325226f0c9ab3fdf8ee4b980909394852f6
Nityam Bhojani <78067067+niityam@users.noreply.github.com>
2022-11-26 19:05:02 +0530

    Kruskal Algorithm

---
+++ SEM-3-DS/Practical-16.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int i, j, k, a, b, u, v, n, ne = 1;
+int min, mincost = 0, cost[9][9], parent[9];
+int find(int);
+int uni(int, int);
+int main() {
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            scanf("%d", &cost[i][j]);
+            if (cost[i][j] == 0)
+                cost[i][j] = 999;
+        }
+    }
+    while (ne < n) {
+        for (i = 1, min = 999; i <= n; i++) {
+            for (j = 1; j <= n; j++) {
+                if (cost[i][j] < min) {
+                    min = cost[i][j];
+                    a = u = i;
+                    b = v = j;
+                }
+            }
+        }
+        u = find(u);
+        v = find(v);
+        if (uni(u, v)) {
+            ne++;
+            mincost += min;
+        }
+        cost[a][b] = cost[b][a] = 999;
+    }
+    printf("Total cost : %d", mincost);
+    return 0;
+}
+int find(int i) {
+    while (parent[i])
+        i = parent[i];
+    return i;
+}
+int uni(int i, int j) {
+    if (i != j) {
+        parent[j] = i;
+        return 1;
+    }
+    return 0;
+}
