397e6a5a0b5af3a32d6a46e875d723ac0bec466b
Nityam Bhojani <78067067+niityam@users.noreply.github.com>
2023-01-21 00:23:15 +0530

    Random Number: Lottery 10k, 5k, 2k, 1k.
    2. Suppose you want to develop a program to play a lottery. The program
    randomly generates a two-digit number, prompts the user to enter a two-
    digit number, and determines whether the user wins according to the
    following rules:
    a. If the user’s input matches the lottery in the exact order, the award is
    $10,000.
    b. If all the digits in the user’s input match all the digits in the lottery
    number, the award is $5,000.
    c. If one digit in the user’s input matches a digit in the lottery number,
    the award is $2,000

--- SEM-4-FP/prac02_2.py
+++ SEM-4-FP/prac02_2.py
@@ -1,23 +1,26 @@
 import random
 val = input("Enter 2 digit number")
-randnum = random.randrange(10,99)
+randnum = str(random.randrange(10,99))
 
 def fn2(val, randnum):
     total =0
+    if(val[0] == val[1] or randnum[0] == randnum[1]):
+        total = total - 1
     for x in val:
         for y in str(randnum):
             if(x == y):
                 total = total +1
     return total
 
-if(val == randnum):
+
+if(int(val) == int(randnum)):
     print(randnum)
-    print("You Won $10,000, The Random Number was",randnum)
+    print("You Won $10,000. The Random Number was",randnum)
 
 elif(fn2(val, randnum) == 2):
-    print("You won $5,000, The Random Number was",randnum)
+    print("You won $5,000. The Random Number was",randnum)
 
 elif(fn2(val, randnum) == 1):
-    print("You won $2,000, The Random Number was",randnum)
+    print("You won $2,000. The Random Number was",randnum)
 else:
-    print("Better Luck Next Time, The Random Number was",randnum)
+    print("Better Luck Next Time. The Random Number was",randnum)
