027faf1cf5750a8bd89fe3e24a2c61565ac251c3
Nityam Bhojani <78067067+niityam@users.noreply.github.com>
2023-01-27 23:17:18 +0530

    Using GDB to debug code

---
+++ SEM-4-OS/Prac1/Prac2/segmentation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{ 
+int i=0;
+i=i+1;
+printf("%d",i);
+
+i=i+1;
+printf("%s",i);
+
+return 0; 
+}
+
---
+++ SEM-4-OS/Prac1/Prac2/stacktrace.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main() {
+    int stack_array[10];
+    stack_array[11] = 35;
+}
+
