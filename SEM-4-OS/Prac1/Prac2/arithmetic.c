784be3fef2859b0019ae4f39ca74230242642ad9
Nityam Bhojani <78067067+niityam@users.noreply.github.com>
2023-01-27 23:17:43 +0530

    Delete SEM-4-OS/Prac1/Prac2 directory

--- SEM-4-OS/Prac1/Prac2/arithmetic.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-void main()
-{
-	int a=10, b=0, c;
-	c =a/b;
-	printf("%d",c);
-}
--- SEM-4-OS/Prac1/Prac2/stacktrace.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-void main() {
-    int stack_array[10];
-    stack_array[11] = 35;
-}
-
